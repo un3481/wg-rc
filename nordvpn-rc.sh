1d3f31f0b6e813367bf5d4b85eeecc0d7ee16db8
un3481 <78743473+un3481@users.noreply.github.com>
2023-11-14 19:19:25 -0300

    add verify_root function

--- nordvpn-rc.sh
+++ nordvpn-rc.sh
@@ -32,11 +32,17 @@ NORDVPN_API_BASE="https://api.nordvpn.com"
 NORDVPN_API_CURRENT_USER="$NORDVPN_API_BASE/v1/users/current"
 NORDVPN_API_CREDENTIALS="$NORDVPN_API_BASE/v1/users/services/credentials"
 
-# Print error message for invalid token and exit
-exit_invalid_token() {
-	echo -e "You have not set a valid nordvpn access token."
-	echo -e "Please follow the instructions provided in their website to obtain a new token: https://support.nordvpn.com/Connectivity/Linux/1905092252/How-to-log-in-to-NordVPN-on-Linux-with-a-token.htm"
-	echo -e "Once you have copied your token, you can set it in nordvpn-rc by running 'nordvpn-rc set --token <your-token-goes-here>'."
+# verify if user has root privileges
+verify_root() {
+	if [ "$(id -u)" != "0" ]; then
+   		exit_non_root
+	fi
+}
+
+# print error message for insufficient privileges and exit
+exit_non_root() {
+	echo -e "This command requires administrative privileges."
+	echo -e "Try 'sudo nordvpn-rc <your-args-go-here>' or login as root."
 	exit 1
 }
 
@@ -51,27 +57,37 @@ verify_token() {
 	# check for errors in response
 	valid="valid"
 	if [[ "$response" == "" ]]; then
-		valid="invalid"
-	fi	
+		exit_invalid_token
+	fi
 	errors=$(printf %s "$response" | jq --raw-output '.errors')
 	if [[ "$errors" != "" ]] && [[ "$errors" != "null" ]]; then
-		valid="invalid"
+		exit_invalid_token
 	fi
-	
-	printf %s "$valid"
+}
+
+# print error message for invalid token and exit
+exit_invalid_token() {
+	echo -e "You have not set a valid nordvpn access token."
+	echo -e "Please follow the instructions provided in their website to obtain a new token: https://support.nordvpn.com/Connectivity/Linux/1905092252/How-to-log-in-to-NordVPN-on-Linux-with-a-token.htm"
+	echo -e "Once you have copied your token, you can set it in nordvpn-rc by running 'nordvpn-rc set --token <your-token-goes-here>'."
+	exit 1
 }
 
 # get wireguard private key from nordvpn and put it in the config file
 # requires superuser privileges
 update_private_key() {
+	verify_root
+
 	local config token response errors private_key
 	
 	# read config file
-	mkdir -p "$NORDVPN_DIR"
-	config=$(cat "$NORDVPN_CONFIG" || "{}")
+	config=$(cat "$NORDVPN_CONFIG" 2>/dev/null || echo "{}")
 
 	# get nordvpn token from config file
 	token=$(printf %s "$config" | jq --raw-output '.token')
+
+	# verify token
+	verify_token "$token"
 
 	# request api for credentials
 	response=$(curl -s "$NORDVPN_API_CREDENTIALS" -u "token:$token")
@@ -95,6 +111,7 @@ update_private_key() {
 	config=$(printf %s "$config" | jq --arg key "$private_key" --raw-output '. | .private_key = $key')
 
 	# write updated config file
+	mkdir -p "$NORDVPN_DIR"
 	echo "$config" > "$NORDVPN_CONFIG"
 	chmod 600 "$NORDVPN_CONFIG"
 	
@@ -103,6 +120,8 @@ update_private_key() {
 # set nordvpn access token in the config file
 # requires superuser privileges
 set_token() {
+	verify_root
+
 	local config token valid
 	token=$1
 
@@ -112,19 +131,16 @@ set_token() {
 	fi
 	
 	# verify token
-	valid=$(verify_token "$token")
-	if [[ "$valid" != "valid" ]]; then
-		exit_invalid_token
-	fi
+	verify_token "$token"
 
 	# read config file
-	mkdir -p "$NORDVPN_DIR"	
-	config=$(cat "$NORDVPN_CONFIG" || "{}")
+	config=$(cat "$NORDVPN_CONFIG" 2>/dev/null || echo "{}")
 	
 	# create new config
 	config=$(printf %s "$config" | jq --arg tok "$token" --raw-output '. | .token = $tok')
 	
 	# write updated config file
+	mkdir -p "$NORDVPN_DIR"
 	echo "$config" > "$NORDVPN_CONFIG"
 	chmod 600 "$NORDVPN_CONFIG"
 
@@ -135,27 +151,26 @@ set_token() {
 # set wireguard interface in the config file
 # requires superuser privileges
 set_interface() {
+	verify_root
+	
 	local config interface
 	interface=$1
 
 	# check the string provided by user
 	if [[ "$interface" == "" ]]; then
 		echoexit "error: Attempting to set invalid interface."
-	fi	
+	fi
 
 	# read config file
-	mkdir -p "$NORDVPN_DIR"	
-	config=$(cat "$NORDVPN_CONFIG" || "{}")
+	config=$(cat "$NORDVPN_CONFIG" 2>/dev/null || echo "{}")
 	
 	# create new config
 	config=$(printf %s "$config" | jq --arg if "$interface" --raw-output '. | .interface = $if')
 	
 	# write updated config file
+	mkdir -p "$NORDVPN_DIR"
 	echo "$config" > "$NORDVPN_CONFIG"
 	chmod 600 "$NORDVPN_CONFIG"
-
-	# update credentials
-	update_private_key
 }
 
 # main
